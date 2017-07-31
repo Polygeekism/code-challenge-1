3a0e460ecbb3b932bad2568a5f7f94f5fcd5caf7
polygeekism <mcmahon.kevin.t@gmail.com>
2017-07-31 13:30:06 -0500

    button now generates a number inside of the button equal to how many times generate has been clicked

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 console.log('script has been sourced');
 
-
+var howManyClicks =0;
 
 $(document).ready(function(){
     //use a click event to add a div to the DOM when generate is clicked
     $('body').on('click', '.buttonGenerate', function(){
-        $('body').append('<div></div>');
+        howManyClicks++;
+        $('body').append('<div>'+ '<p>' + howManyClicks +
+        '</p>' + '</div>');
     });
     //add a button labeled generate to the DOM
     $('body').append('<button class="buttonGenerate">Generate</button>');
