d24a6f0eca6e1679c3cc2381cb48a7e5378b3c98
polygeekism <mcmahon.kevin.t@gmail.com>
2017-07-31 13:27:37 -0500

    generate button adds a blank div to the body on the DOM

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ console.log('script has been sourced');
 
 
 $(document).ready(function(){
+    //use a click event to add a div to the DOM when generate is clicked
+    $('body').on('click', '.buttonGenerate', function(){
+        $('body').append('<div></div>');
+    });
+    //add a button labeled generate to the DOM
     $('body').append('<button class="buttonGenerate">Generate</button>');
 });
